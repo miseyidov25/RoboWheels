33e75758aca7d33745a2410cd365f1b26f990aa6
MirIsmayil <miseyidov25@gmail.com>
2025-10-02 14:47:08 +0200

    Added button 21 (integrated button) for testing

--- src/main.c
+++ src/main.c
@@ -4,9 +4,9 @@
 #include "buttons.h"
 #include "echo.h"
 
-// Pins
+// Echo sensor pins
 #define TRIG 8
-#define ECHO 7
+#define ECHO 9
 #define STOP_DISTANCE 10
 
 void setup() {
@@ -20,14 +20,23 @@ void loop() {
     buttons_update();
     unsigned long now = millis();
 
-    bool fwd = is_forward_pressed();
-    bool rev = is_reverse_pressed();
-    bool left = is_left_pressed();
+    // Normal button states
+    bool fwd   = is_forward_pressed();
+    bool rev   = is_reverse_pressed();
+    bool left  = is_left_pressed();
     bool right = is_right_pressed();
 
+    // Test button override (pin 21 toggle)
+    if (is_test_forward_active()) {
+        fwd = true;
+        rev = false;
+    }
+
+    // Echo distance
     int distance = echo_getDistance();
     bool obstacle = (distance > 0 && distance <= STOP_DISTANCE);
 
+    // Update motors + LEDs
     motors_update(fwd, rev, left, right, obstacle);
     leds_update(now, fwd, rev, left, right);
 }
