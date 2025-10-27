9d08d3cf08b51979109e902fca17096453c47b47
Igor-Szab <szablewskiigor@gmail.com>
2025-10-27 11:56:24 +0100

    feat: line sensor code

---
+++ include/line.h
@@ -0,0 +1,10 @@
+#ifndef LINE_H
+#define LINE_H
+
+#include <Arduino.h>
+
+void line_init(left, right, middle, LINE_LED);
+void line_update();
+
+#endif
+
--- include/motors.h
+++ include/motors.h
@@ -4,11 +4,15 @@
 #include <Arduino.h>
 
 void motors_init();
+void motors_set_speed(int speed);
 void motors_forward();
 void motors_reverse();
 void motors_left();
 void motors_right();
 void motors_brake();
 void motors_coast();
+
+// High-level update function for main loop
+void motors_update(bool fwd);
 
 #endif
