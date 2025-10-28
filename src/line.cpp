aff3c4fa55830efe5752ae817e29141fa98817e8
Igor-Szab <szablewskiigor@gmail.com>
2025-10-28 12:32:00 +0100

    feat: line update

--- src/line.cpp
+++ src/line.cpp
@@ -3,9 +3,9 @@
 #include "buttons.h"
 #include "line.h"
 
-#define left 12
-#define right 10
-#define middle 11
+#define left 13
+#define right 11
+#define middle 12
 #define LINE_LED 13
 
 static unsigned long previousMillis = 0;
@@ -24,45 +24,52 @@ void line_update(){
   bool rightSensor = digitalRead(right);
   unsigned long currentMillis = millis();
 
-  if(leftSensor == 0 && middleSensor == 0 && rightSensor == 0){  // All sensors on line - on
+  if(leftSensor == 0 && middleSensor == 0 && rightSensor == 0){  // All sensors on line - forward
     digitalWrite(LINE_LED, HIGH);
+    motors_forward();
   }
-  else if(leftSensor == 1 && middleSensor == 1 && rightSensor == 1){  // All sensors off line - off
+  else if(leftSensor == 1 && middleSensor == 1 && rightSensor == 1){  // All sensors off line - stop
     digitalWrite(LINE_LED, LOW);
+    motors_brake();
   }
-  else if(leftSensor == 0 && middleSensor == 0 && rightSensor == 1){  // Right sensor off line - blink (100ms)
+  else if(leftSensor == 0 && middleSensor == 0 && rightSensor == 1){  // Right sensor off line - turn left
     if(currentMillis - previousMillis >= 100){
       previousMillis = currentMillis;
       ledState = !ledState;
       digitalWrite(LINE_LED, ledState);
     }
+    motors_left();
   }
-  else if(leftSensor == 1 && middleSensor == 0 && rightSensor == 0){  // Left sensor off line - blink (500ms)
+  else if(leftSensor == 1 && middleSensor == 0 && rightSensor == 0){  // Left sensor off line - turn right
     if(currentMillis - previousMillis >= 500){
       previousMillis = currentMillis;
       ledState = !ledState;
       digitalWrite(LINE_LED, ledState);
     }
+    motors_right();
   }
-  else if(leftSensor == 1 && middleSensor == 0 && rightSensor == 1){  // Only middle on line - blink (300ms)
+  else if(leftSensor == 1 && middleSensor == 0 && rightSensor == 1){  // Only middle on line - forward
     if(currentMillis - previousMillis >= 300){
       previousMillis = currentMillis;
       ledState = !ledState;
       digitalWrite(LINE_LED, ledState);
     }
+    motors_forward();
   }
-  else if(leftSensor == 0 && middleSensor == 1 && rightSensor == 1){  // Only left on line - blink (200ms)
+  else if(leftSensor == 0 && middleSensor == 1 && rightSensor == 1){  // Only left on line - turn left
     if(currentMillis - previousMillis >= 200){
       previousMillis = currentMillis;
       ledState = !ledState;
       digitalWrite(LINE_LED, ledState);
     }
+    motors_left();
   }
-  else if(leftSensor == 1 && middleSensor == 1 && rightSensor == 0){  // Only right on line - blink (400ms)
+  else if(leftSensor == 1 && middleSensor == 1 && rightSensor == 0){  // Only right on line - turn 
     if(currentMillis - previousMillis >= 400){
       previousMillis = currentMillis;
       ledState = !ledState;
       digitalWrite(LINE_LED, ledState);
     }
+    motors_right();
   }
 }
