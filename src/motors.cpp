c6f1d91d90007e5bc8438712f15116c0de7bb376
Igor-Szab <szablewskiigor@gmail.com>
2025-12-02 12:43:17 +0100

    feat/motor fix

--- src/motors.cpp
+++ src/motors.cpp
@@ -45,20 +45,16 @@ void motors_reverse() {
 }
 void motors_left() {
   int s = currentEffectiveSpeed();
-  // Turn left by reversing left motor and driving right motor forward
-  analogWrite(IN1, 0);      // left motor forward pin off
-  analogWrite(IN2, s);      // left motor reverse pin on
-  analogWrite(IN3, s);      // right motor forward pin on
-  analogWrite(IN4, 0);      // right motor reverse pin off
+  analogWrite(IN1, 0);
+  analogWrite(IN2, s);
+  analogWrite(IN3, s);
+  analogWrite(IN4, 0);
   motorDirection = 2;
 }
 void motors_correctleft() {
-  // Slight left correction: reduce left forward contribution (soft turn)
+  // Slight left turn - reduce left side speed
   int s = currentEffectiveSpeed();
-  int leftSpeed = s - 30;
-  if (leftSpeed < 0) leftSpeed = 0;
-  // Keep left motor forward reduced, right motor forward normal
-  analogWrite(IN1, leftSpeed);
+  analogWrite(IN1, s - 30);
   analogWrite(IN2, 0);
   analogWrite(IN3, s);
   analogWrite(IN4, 0);
@@ -66,22 +62,19 @@ void motors_correctleft() {
 }
 void motors_right() {
   int s = currentEffectiveSpeed();
-  // Turn right by driving left motor forward and reversing right motor
-  analogWrite(IN1, s);    // left motor forward
-  analogWrite(IN2, 0);    // left motor reverse off
-  analogWrite(IN3, 0);    // right motor forward off
-  analogWrite(IN4, s);    // right motor reverse
+  analogWrite(IN1, s);
+  analogWrite(IN2, 0);
+  analogWrite(IN3, 0);
+  analogWrite(IN4, s);
   motorDirection = 3;
 }
 void motors_correctright() {
-  // Slight right correction: reduce right forward contribution (soft turn)
+  // Slight right turn - reduce right side speed
   int s = currentEffectiveSpeed();
-  int rightSpeed = s - 30;
-  if (rightSpeed < 0) rightSpeed = 0;
   analogWrite(IN1, s);
   analogWrite(IN2, 0);
-  analogWrite(IN3, rightSpeed);
-  analogWrite(IN4, 0);
+  analogWrite(IN3, 0);
+  analogWrite(IN4, s - 30);
   motorDirection = 3;
 }
 void motors_brake() {
